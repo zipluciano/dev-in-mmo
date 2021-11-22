c1c748c4b5d2540a623bd395fe32e452ba890a09
Luciano E. Monteiro <luciano.es.mo@gmail.com>
2021-11-21 23:54:14 -0300

    Fixing errors 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,8 @@
 import React, { useContext } from 'react';
 import { NavBarHeader, NavBarList } from './Header.styles';
 import { Link } from 'react-router-dom';
-import { Theme } from '../../providers/providers';
-import { FaMoon, FaSun } from 'react-icons/fa';
 
 const Header = () => {
-	const { light, setLight } = useContext(Theme);
-	console.log(light);
-
 	return (
 		<NavBarHeader>
 			<nav>
@@ -22,13 +17,6 @@ const Header = () => {
 						<Link to='/news' style={{ textDecoration: 'none', color: '#fff' }}>
 							Notícias
 						</Link>
-					</li>
-					<li>
-						{light === true ? (
-							<FaMoon onClick={() => setLight((prev) => !prev)} />
-						) : (
-							<FaSun onClick={() => setLight((prev) => !prev)} />
-						)}
 					</li>
 				</NavBarList>
 			</nav>
--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -2,21 +2,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GameList from '../pages/GameList/GameList';
 import GamePage from '../pages/GamePage/GamePage';
 import News from '../pages/News/News';
-import { useState } from 'react';
-import { Theme } from '../../providers/providers';
 
 const Routes = () => {
-	const [light, setLight] = useState(true);
-
 	return (
 		<Router>
-			<Theme.Provider value={{ light, setLight }}>
-				<Switch>
-					<Route exact path='/' component={GameList} />
-					<Route exact path='/game/:id' component={GamePage} />
-					<Route exact path='/news' component={News} />
-				</Switch>
-			</Theme.Provider>
+			<Switch>
+				<Route exact path='/' component={GameList} />
+				<Route exact path='/game/:id' component={GamePage} />
+				<Route exact path='/news' component={News} />
+			</Switch>
 		</Router>
 	);
 };
